6e84ce6f182a06ecaddaf125026c87ddf59a2a3c
leangDev <ngounmengleang18@gmail.com>
2022-04-23 15:02:14 +0700

    user login

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function login(Request $request){
+        $user = User::where('email',$request->email)->first();
+        if($user && Hash::check($request->password,$user->password)){
+            return response()->json(
+                [
+                    "status" => "ok",
+                    "token" => $user->createToken($user->id)->plainTextToken
+                ],400
+            );
+        }else{
+            return response()->json(
+                [
+                    "status" => "invalid email or password"
+                ],403
+            );
+        }
+    }
+}
