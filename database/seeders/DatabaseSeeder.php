a13688f022a48fc6ee86f4535cad2c22c8be4895
leangDev <ngounmengleang18@gmail.com>
2022-04-23 16:53:36 +0700

    remove facker from dev depen

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,11 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->count(10)->create();
         $this->call(
             [
                 FacultySeeder::class,
-                DepartmentSeeder::class
+                DepartmentSeeder::class,
+                UserSeeder::class
             ]
         );
     }
