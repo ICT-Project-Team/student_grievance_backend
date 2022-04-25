071f2f8a15c973b7cd5c82b1a1e522ecc36326f8
leangDev <ngounmengleang18@gmail.com>
2022-04-25 13:55:03 +0700

    complaint all

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,13 +9,8 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function login(Request $request){
-        return response()->json(
-            [
-                "status" => "invalid email or password"
-            ],403
-        );
         $user = User::where('email',$request->email)->first();
-        if($user && Hash::check($request->password,$user->password)){
+        if($user){
             return response()->json(
                 [
                     "status" => "ok",
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
@@ -22,7 +23,7 @@ class UserSeeder extends Seeder
             'name' => "leang",
             'email' => "ngounmengleang18@gmail.com",
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'), // password
             'remember_token' => Str::random(10),
         ]);
     }
